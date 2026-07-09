cc49c788cea2ed4c51486467ee639cdf9dfbfac3
xn2pr42e-dev <xn2pr42e@gmail.com>
2026-07-09 00:09:15 +0000

    تحديث Arab Chat

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 const http = require("http");
+const fs = require("fs");
+const WebSocket = require("ws");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Chat Server is running");
+    fs.readFile("index.html", (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("Error");
+            return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+    console.log("User connected");
+
+    ws.on("message", (message) => {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        console.log("User disconnected");
+    });
 });
 
 server.listen(3000, () => {
-    console.log("Server started on port 3000");
+    console.log("Chat server started on port 3000");
 });
